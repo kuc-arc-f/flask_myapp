a5753635dccd632be8b209e96645777de8bd3f29
kuc-arc-f <naka@kuc-arc-f.com>
2019-02-19 16:52:21 +0900

    up,0219A11

--- myapp/app.py
+++ myapp/app.py
@@ -7,5 +7,6 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=80, debug=True)
+    app.run(host='localhost',  debug=True)
+    #app.run(host='localhost', port=80, debug=True)
     #app.run()
